b300975a86cc016127ef1f3c8c2ea48e69d59663
Shubam Mishra <shubhammishra10101998@gmail.com>
2023-12-21 20:28:45 +0530

    Creates useful components

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,9 @@
+import styles from './features.module.css';
+
+const Featured = () => {
+  return (
+    <div className={styles.container}>Featured</div>
+  )
+}
+
+export default Featured
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import styles from './navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.container}>Navbar</nav>
+  )
+}
+
+export default Navbar
